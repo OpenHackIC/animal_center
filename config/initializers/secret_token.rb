c0b4ab9ad857449cda8b351896502724490d56e3
Benjamin Oakes <hello@benjaminoakes.com>
2013-05-27 22:23:01 -0500

    $ rails new animal_center

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimalCenter::Application.config.secret_token = 'a286283d033817b145153bd5085a7689777fbf4a54391edc0084b6799732817b1495cc9f6cb4f66b680113d868c123e22d38a4fa3562f90378e363a975bbd244'
