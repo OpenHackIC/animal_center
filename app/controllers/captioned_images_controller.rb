1033428bfbdb95effca0602d5b3ed157fd99ec93
Benjamin Oakes <hello@benjaminoakes.com>
2013-06-02 14:51:59 -0500

    Disallow edit and destroy

--- app/controllers/captioned_images_controller.rb
+++ app/controllers/captioned_images_controller.rb
@@ -32,11 +32,6 @@ class CaptionedImagesController < ApplicationController
     end
   end
 
-  # GET /captioned_images/1/edit
-  def edit
-    @captioned_image = CaptionedImage.find(params[:id])
-  end
-
   # POST /captioned_images
   # POST /captioned_images.json
   def create
@@ -50,34 +45,6 @@ class CaptionedImagesController < ApplicationController
         format.html { render action: "new" }
         format.json { render json: @captioned_image.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PUT /captioned_images/1
-  # PUT /captioned_images/1.json
-  def update
-    @captioned_image = CaptionedImage.find(params[:id])
-
-    respond_to do |format|
-      if @captioned_image.update_attributes(params[:captioned_image])
-        format.html { redirect_to @captioned_image, notice: 'Captioned image was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @captioned_image.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /captioned_images/1
-  # DELETE /captioned_images/1.json
-  def destroy
-    @captioned_image = CaptionedImage.find(params[:id])
-    @captioned_image.destroy
-
-    respond_to do |format|
-      format.html { redirect_to captioned_images_url }
-      format.json { head :no_content }
     end
   end
 end
