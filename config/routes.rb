20af9386d1be3a7e5b217bdd6ac209414ad8564c
Benjamin Oakes <hello@benjaminoakes.com>
2013-06-02 14:58:40 -0500

    Root is now captioned_images#index
    Closes #9

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 AnimalCenter::Application.routes.draw do
   resources :captioned_images
 
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,7 +50,7 @@ AnimalCenter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'captioned_images#index'
 
   # See how all your routes lay out with "rake routes"
 
