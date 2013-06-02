6459770b262365c9ec22a23c1989d62a535b1360
DariaNico <grassneedle@yahoo.com>
2013-06-02 17:04:48 -0400

    Update source_image.rb

--- app/models/source_image.rb
+++ app/models/source_image.rb
@@ -1,4 +1,8 @@
 class SourceImage < ActiveRecord::Base
   attr_accessible :category, :external_id, :url
   has_many :captioned_images
+  
+def makeString (id=source_image.external_id) 
+  puts "http://www.icanimalcenter.org/indexa.php?id=#{id}"
+end
 end
