94277a9ab73f3247279d98027f0144f3a2f7bf38
Benjamin Oakes <hello@benjaminoakes.com>
2013-06-02 20:11:14 -0500

    Add link for adoption_info_url
    Closes #13

--- app/models/captioned_image.rb
+++ app/models/captioned_image.rb
@@ -1,15 +1,15 @@
 class CaptionedImage < ActiveRecord::Base
   attr_accessible :source_image_id, :text_bottom, :text_top, :title
   belongs_to :source_image
+  delegate :adoption_info_url, to: :source_image
 
   def image_url
     SimpleMemeCaptainClient.captioned_image_url(u: source_image.url,
                                                 t1: text_top,
                                                 t2: text_bottom)
   end
-                                                
+ 
   def to_param
     "#{id}-#{title.parameterize}"
   end
-
 end
